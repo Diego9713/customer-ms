9acd0bdf8deb48c3b34136ac10df18c6db7adfe4
Diego Santiago Tafur Sanchez <dtafursa@emeal.nttdata.com>
2022-01-22 14:03:32 -0500

    hotfix test

--- src/main/java/bootcamp/com/customerms/utils/AppUtil.java
+++ src/main/java/bootcamp/com/customerms/utils/AppUtil.java
@@ -3,6 +3,7 @@ package bootcamp.com.customerms.utils;
 import bootcamp.com.customerms.model.Customer;
 import bootcamp.com.customerms.model.CustomerDto;
 import org.springframework.beans.BeanUtils;
+import reactor.core.publisher.Mono;
 
 /**
  * Method to modify the customer attributes.
@@ -23,4 +24,46 @@ public class AppUtil {
     BeanUtils.copyProperties(customer, customerDto);
     return customerDto;
   }
+
+  /**
+   * Method to modify the return of data.
+   *
+   * @param customerDto -> is object with data complete.
+   * @return object customer modified.
+   */
+  public static Customer productDtoToEntity(CustomerDto customerDto) {
+    Customer customer = new Customer();
+    BeanUtils.copyProperties(customerDto, customer);
+    return customer;
+  }
+
+  /**
+   * Method to create object the return of data.
+   *
+   * @param customer -> is object with data complete.
+   * @return object customer modified.
+   */
+  public static Mono<CustomerDto> createObjectCustomer(CustomerDto customer) {
+    CustomerDto customerDto = new CustomerDto();
+    BeanUtils.copyProperties(customer, customerDto);
+    customerDto.setCustomerType(customerDto.getCustomerType().toUpperCase());
+    customerDto.setStatus(ConstantsCustomer.ACTIVE.name());
+    return Mono.just(customerDto);
+  }
+
+  /**
+   * Method to update object the return of data.
+   *
+   * @param customer -> is object with data complete.
+   * @return object customer modified.
+   */
+  public static Mono<CustomerDto> updateObjectCustomer(CustomerDto customer,String id) {
+    CustomerDto customerDto = new CustomerDto();
+    BeanUtils.copyProperties(customer, customerDto);
+    customerDto.setCustomerType(customerDto.getCustomerType().toUpperCase());
+    customerDto.setId(id);
+    customerDto.setStatus(ConstantsCustomer.ACTIVE.name());
+    return Mono.just(customerDto);
+  }
+
 }
